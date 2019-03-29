a53ebd9c3261815ca4168b1b164dadc678d598f0
Antavio <njuguna13@gmail.com>
2019-03-29 17:17:39 +0300

    Added promises to the project.

--- src/app/profile-request/profile.service.ts
+++ src/app/profile-request/profile.service.ts
@@ -18,10 +18,18 @@ export class ProfileService {
       
 
   }
-     return this.http.get<UserInfo>(environment.apiUrl+environment.access_token).subscribe(data=>{
-      this.user.login= data.login
-      console.log(data.login);
-     })
+
+    let promise = new Promise((resolve,reject)=>{
+      this.http.get<UserInfo>(environment.apiUrl+environment.access_token).subscribe(data=>{
+        this.user.login= data.login
+        // console.log(data.login);
+        resolve()
+       },error=>{
+         this.user.login = "No Name"
+         reject(error)
+       })
+    })
+     return promise
    }
 }
 // "https://api.github.com/users/Antavio?access_token=d0516c8c29f1f42895f969ce6cac48ba41506d23"
