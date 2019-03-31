87f06281430be31fab8849ce2e74c349fc40b779
Antavio <njuguna13@gmail.com>
2019-03-31 17:18:06 +0300

    Added functionality to custom pipe.

--- src/app/upper-case.pipe.ts
+++ src/app/upper-case.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class UpperCasePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: string): any {
+    return value.toUpperCase();
   }
 
 }
